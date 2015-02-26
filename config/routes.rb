e340e8f05c2a3bc66dcbdbba7b5a077514436090
Klas Eskilson <klas.eskilson@gmail.com>
2015-02-26 16:35:54 +0100

    Add pages controller

--- config/routes.rb
+++ config/routes.rb
@@ -2,10 +2,10 @@ Rails.application.routes.draw do
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
-  # root 'welcome#index'
+  root 'pages#show', page: 'home'
 
   # recieve auth callback
-  match '/auth/:provider/callback', to: 'sessions#create'
+  match '/auth/:provider/callback', via: [:get, :post], to: 'sessions#create'
   # logout
-  match '/signout', to: 'sessions#destroy'
+  match '/signout', via: [:get, :post], to: 'sessions#destroy'
 end
