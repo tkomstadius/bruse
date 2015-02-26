bcc4bf5d55a9bbe3b757c143a92af0e7d6a41e9a
Klas Eskilson <klas.eskilson@gmail.com>
2015-02-26 16:34:45 +0100

    Add SessionController
    The idea is that this controller should handle all our sessions
    and incoming responses from remote oauth authentications.

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,20 @@
+class SessionsController < ApplicationController
+  def create
+    session[:user_token] = auth_hash[:credentials][:token]
+    flash[:notice] = "Logged in"
+
+    redirect_to root_url
+  end
+
+  def destroy
+    session[:user_token] = nil
+    flash[:notice] = "Logged out"
+
+    redirect_to root_url
+  end
+
+  private
+    def auth_hash
+      request.env['omniauth.auth']
+    end
+end
