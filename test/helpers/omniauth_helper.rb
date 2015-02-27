cf069d71fdd4f71a9b0ed97bee9b502300e3a55a
danielronnkvist <daniel@ronnkvist.com>
2015-02-27 16:50:04 +0100

    Moved test hashes to helper methods

--- test/helpers/omniauth_helper.rb
+++ test/helpers/omniauth_helper.rb
@@ -1,28 +1,33 @@
+require "test_helper"
 OmniAuth.config.test_mode = true
-omniauth_hash_new = { 'provider' => 'dropbox_oauth2',
-                      'info' => {
-                          'name' => 'Kalle Bar',
-                          'email' => 'kalle@bar.com',
-                          'uid' => '666666'
-                      },
-                      'credentials' => {
-                        'token' => '1234xyz666abc'
-                      }
-}
 
-omniauth_hash_old = { 'provider' => 'dropbox_oauth2',
-                      'info' => {
-                          'name' => 'Foo Bar',
-                          'email' => 'foo@bar.com',
-                          'uid' => '666123'
-                      },
-                      'credentials' => {
-                        'token' => 'xyz123'
-                      }
-}
+def dropbox_new
+  omniauth_hash_new = { 'provider' => 'dropbox_oauth2',
+                        'info' => {
+                            'name' => 'Kalle Bar',
+                            'email' => 'kalle@bar.com',
+                            'uid' => '666666'
+                        },
+                        'credentials' => {
+                          'token' => '1234xyz666abc'
+                        }
+  }
+  OmniAuth.config.add_mock(:dropbox_oauth2, omniauth_hash_new)
+end
 
-OmniAuth.config.add_mock(:dropbox_new, omniauth_hash_new)
-OmniAuth.config.add_mock(:dropbox_old, omniauth_hash_old)
+def dropbox_old
+  omniauth_hash_old = { 'provider' => 'dropbox_oauth2',
+                        'info' => {
+                            'name' => 'Foo Bar',
+                            'email' => 'foo@bar.com',
+                            'uid' => '666123'
+                        },
+                        'credentials' => {
+                          'token' => 'xyz123'
+                        }
+  }
+  OmniAuth.config.add_mock(:dropbox_oauth2, omniauth_hash_old)
+end
 
 def log_in_user(user_id)
   session[:user] = user_id
