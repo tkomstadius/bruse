54be8b848859c49bd86e310c83ea79c00c3dc801
danielronnkvist <daniel@ronnkvist.com>
2015-02-27 09:52:25 +0100

    Added user id to identity model
    Since the token may have the ability to change we would like a static reference as well

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150227082736) do
+ActiveRecord::Schema.define(version: 20150227085136) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -22,6 +22,7 @@ ActiveRecord::Schema.define(version: 20150227082736) do
     t.string   "service"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string   "uid"
   end
 
   add_index "identities", ["user_id"], name: "index_identities_on_user_id", using: :btree
