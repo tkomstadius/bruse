51b3f3c9e915c4d5123a32ed6b52875dc0683a00
danielronnkvist <daniel@ronnkvist.com>
2015-02-27 16:51:35 +0100

    Uses the new omniauth helper methods for testing models

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -4,27 +4,27 @@ require 'helpers/omniauth_helper'
 class UserTest < ActiveSupport::TestCase
   test "create from oauth" do
     num_users = User.count
-    auth = OmniAuth.config.mock_auth[:dropbox_new]
+    auth = dropbox_new
     user = User.find_or_create_from_oauth(auth)
-    user = User.find(user)
 
     assert_equal num_users+1, User.count
     assert_equal user["name"], auth["info"]["name"]
   end
 
   test "sign in user from oauth" do
+    users(:one)
+    identities(:one)
     num_users = User.count
-    auth = OmniAuth.config.mock_auth[:dropbox_old]
+    auth = dropbox_old
     user = User.find_or_create_from_oauth(auth)
-    user = User.find(user)
 
     assert_equal num_users, User.count
     assert_equal user["name"], auth["info"]["name"]
+    assert_equal user["name"], users(:one).name
   end
 
   test "tries to sign in without credentials" do
     user = User.find_or_create_from_oauth(nil)
-
     assert_nil user
   end
 end
