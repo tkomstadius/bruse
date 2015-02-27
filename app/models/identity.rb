1a9895c2bd424afdd4ae6b981a1e06d5346869d0
danielronnkvist <daniel@ronnkvist.com>
2015-02-27 10:10:26 +0100

    Functions for finding user and identity from oauth
    Finds or creates user from matching identity.

--- app/models/identity.rb
+++ app/models/identity.rb
@@ -1,3 +1,27 @@
 class Identity < ActiveRecord::Base
   belongs_to :user
+
+  # Public: Creates or finds an identity from oauth information
+  #
+  # auth_hash  - Hash with information from omniauth
+  #
+  # Examples
+  #
+  #   Identity.find_or_create_from_oauth(auth_hash)
+  #   # => Identity
+  #
+  # Returns an identity object
+  def self.find_or_create_from_oauth(auth_hash)
+    identity = self.find_by(:uid => auth_hash[:info][:uid], :service => auth_hash[:provider])
+    if !identity
+      identity = Identity.new(:uid     => auth_hash[:info][:uid],
+                              :token   => auth_hash[:credentials][:token],
+                              :service => auth_hash[:provider])
+    else
+      identity.token = auth_hash[:credentials][:token]
+    end
+    identity.save!
+    identity # return identity
+  end
+
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,35 @@
 class User < ActiveRecord::Base
   has_many :identities
+
+  # Public: Creates or finds a user from oauth information
+  #
+  # auth_hash  - Hash with information from omniauth
+  #
+  # Examples
+  #
+  #   User.find_or_create_from_oauth(auth_hash)
+  #   # => User
+  #
+  # Returns a user object
+  def find_or_create_from_oauth(auth_hash)
+    identity = Identity.find_or_create_from_oauth(auth_hash)
+    user = current_user ? current_user : identity.user
+
+    if user.nil?
+      email = auth_hash[:info][:email]
+      user = User.find_by(:email => email)
+
+      if user.nil?
+        user = User.new(:email => email,
+                        :name  => auth_hash[:info][:name])
+        user.save!
+      end
+    end
+
+    if identity.user != user
+      identity.user = user
+      identity.save!
+    end
+    user # return user
+  end
 end
