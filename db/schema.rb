7ece1e77edb1c222b305d5b3712e6af585310ff5
danielronnkvist <daniel@ronnkvist.com>
2015-02-27 11:14:40 +0100

    Name to identity and nice meta about user
    Each identity should have a name so that a user can distinguish its accounts.
    To keep track of our users we count number of sign ins and when it last signed in.

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150227085136) do
+ActiveRecord::Schema.define(version: 20150227101340) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -23,6 +23,7 @@ ActiveRecord::Schema.define(version: 20150227085136) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string   "uid"
+    t.string   "name"
   end
 
   add_index "identities", ["user_id"], name: "index_identities_on_user_id", using: :btree
@@ -37,8 +38,10 @@ ActiveRecord::Schema.define(version: 20150227085136) do
   create_table "users", force: :cascade do |t|
     t.string   "email"
     t.string   "name"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at",                  null: false
+    t.datetime "updated_at",                  null: false
+    t.integer  "sign_in_count",   default: 0
+    t.datetime "last_sign_in_at"
   end
 
   add_foreign_key "identities", "users"
