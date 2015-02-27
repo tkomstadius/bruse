bd4ee71ca56554daf7c8fe6ba0973f75beb01e19
danielronnkvist <daniel@ronnkvist.com>
2015-02-27 11:38:10 +0100

    Removed old test table

--- db/schema.rb
+++ db/schema.rb
@@ -28,13 +28,6 @@ ActiveRecord::Schema.define(version: 20150227101340) do
 
   add_index "identities", ["user_id"], name: "index_identities_on_user_id", using: :btree
 
-  create_table "posts", force: :cascade do |t|
-    t.string   "title"
-    t.string   "body"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
   create_table "users", force: :cascade do |t|
     t.string   "email"
     t.string   "name"
