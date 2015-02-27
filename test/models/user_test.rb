966e50b96302fc933cf8cf6b727881f54a086a26
danielronnkvist <daniel@ronnkvist.com>
2015-02-27 14:57:04 +0100

    Three simple tests for the user model

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -1,7 +1,30 @@
 require 'test_helper'
+require 'helpers/omniauth_helper'
 
 class UserTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "create from oauth" do
+    num_users = User.count
+    auth = OmniAuth.config.mock_auth[:dropbox_new]
+    user = User.find_or_create_from_oauth(auth)
+    user = User.find(user)
+
+    assert_equal num_users+1, User.count
+    assert_equal user["name"], auth["info"]["name"]
+  end
+
+  test "sign in user from oauth" do
+    num_users = User.count
+    auth = OmniAuth.config.mock_auth[:dropbox_old]
+    user = User.find_or_create_from_oauth(auth)
+    user = User.find(user)
+
+    assert_equal num_users, User.count
+    assert_equal user["name"], auth["info"]["name"]
+  end
+
+  test "tries to sign in without credentials" do
+    user = User.find_or_create_from_oauth(nil)
+
+    assert_nil user
+  end
 end
