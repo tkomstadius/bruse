bfd354820e8969d6f281cf2e1b5e6836a628a791
danielronnkvist <daniel@ronnkvist.com>
2015-03-04 16:04:38 +0100

    Authenticates user before continuing

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -9,4 +9,8 @@ class ApplicationController < ActionController::Base
   def current_user
     @current_user ||= User.find(session[:user_id]) if session[:user_id]
   end
+
+  def authenticate_user!
+    redirect_to root_url if !current_user
+  end
 end
--- app/controllers/identities_controller.rb
+++ app/controllers/identities_controller.rb
@@ -1,4 +1,6 @@
 class IdentitiesController < ApplicationController
+  before_action :authenticate_user!
+
   def destroy
     identity = Identity.find(params["id"])
     if identity.user.identities.count > 1
