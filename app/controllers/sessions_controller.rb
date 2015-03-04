8c19a8124854c62bcb342bc365865bd6dfba98cd
danielronnkvist <daniel@ronnkvist.com>
2015-03-04 10:58:02 +0100

    Store the id instead of the object
    Since the user object can be quite big it's not so good to store it in the session variable.

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,13 +1,13 @@
 class SessionsController < ApplicationController
   def create
-    session[:user] = User.find_or_create_from_oauth(auth_hash, current_user)
+    session[:user_id] = User.find_or_create_from_oauth(auth_hash, current_user).id
 
     flash[:notice] = "Logged in"
     redirect_to root_url
   end
 
   def destroy
-    session[:user] = nil
+    session[:user_id] = nil
 
     flash[:notice] = "Logged out"
     redirect_to root_url
