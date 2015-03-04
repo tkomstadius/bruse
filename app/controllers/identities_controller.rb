77b28f4f3a803b12c3760d054eeeacff66267c69
danielronnkvist <daniel@ronnkvist.com>
2015-03-04 12:49:18 +0100

    If the last identity is removed the user is destroyed
    TODO: Add warning telling the user that the account is going to be destroyed.

--- app/controllers/identities_controller.rb
+++ app/controllers/identities_controller.rb
@@ -1,7 +1,15 @@
 class IdentitiesController < ApplicationController
   def destroy
     identity = Identity.find(params["id"])
-    identity.destroy
-    redirect_to profile_url
+    if identity.user.identities.count > 1
+      identity.destroy
+      redirect_to profile_url
+    else
+      user = identity.user
+      identity.destroy
+      user.destroy
+      session[:user_id] = nil
+      redirect_to root_url
+    end
   end
 end
