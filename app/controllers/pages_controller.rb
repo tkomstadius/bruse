8e7e47667a1742b170fe07ac6df41dca5ae01897
Klas Eskilson <klas.eskilson@gmail.com>
2015-03-05 11:02:40 +0100

    Add flash messages to layout and omniauth error

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -4,6 +4,7 @@ class PagesController < ApplicationController
   end
 
   def omniauth_failure
+    flash[:alert] = "Could not link your account. :( Are you sure you gave us permission to do so?"
     redirect_to root_url
   end
 end
