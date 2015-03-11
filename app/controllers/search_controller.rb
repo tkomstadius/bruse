ccaff9bca2896c4f224aef842346fc0242ef635f
danielronnkvist <daniel@ronnkvist.com>
2015-03-11 13:49:05 +0100

    Fuzzy search using Fuzzily
    Needs tweaking to work with numbers and åäö

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -4,8 +4,8 @@ class SearchController < ApplicationController
     query = params[:query]
     # Initiates array here since ruby is a pass by reference language
     @results = []
-    @results = BruseFile.where(:name => query)
-    tags = Tag.where(:name => query)
+    @results = BruseFile.find_by_fuzzy_name(query)
+    tags = Tag.find_by_fuzzy_name(query)
     tags.each do |tag|
       @results.push(tag.bruse_file) # Get the files from each tag
     end
