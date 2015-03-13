49018cf50c2777faa046c45374d2e1f0c0d7a6cc
danielronnkvist <daniel@ronnkvist.com>
2015-03-13 09:34:21 +0100

    Omniauth failure works again

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -5,6 +5,10 @@ class PagesController < ApplicationController
 
   def omniauth_failure
     flash[:alert] = "Could not link your account. :( Are you sure you gave us permission to do so?"
-    redirect_to root_url
+    if !current_user
+      redirect_to root_url
+    else
+      redirect_to profile_url
+    end
   end
 end
