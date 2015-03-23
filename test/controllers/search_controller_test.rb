086af9f1ba6fb353013f40aeec31c8ca4e103dbc
danielronnkvist <daniel@ronnkvist.com>
2015-03-23 13:37:10 +0100

    Added test for multiple search criteria. Made things easier with helper.

--- test/controllers/search_controller_test.rb
+++ test/controllers/search_controller_test.rb
@@ -1,6 +1,10 @@
 require 'test_helper'
 
 class SearchControllerTest < ActionController::TestCase
+  def files(response)
+    ActiveSupport::JSON.decode(response.body)["files"]
+  end
+
   before do
     BruseFile.bulk_update_fuzzy_name
     Tag.bulk_update_fuzzy_name
@@ -13,26 +17,31 @@ class SearchControllerTest < ActionController::TestCase
 
   test "fuzzy search for a file name" do
     response = post(:find, {:tags => [], :filetypes => [], :fuzzy => [bruse_files(:one).name], :format => :json}, { :user_id => users(:fooBar).id })
-    assert_includes response.body, bruse_files(:one).name
+    assert_equal files(response).first["name"], bruse_files(:one).name
   end
 
   test "search with empty parameters" do
     response = post(:find, {:tags => [], :filetypes => [], :fuzzy => [], :format => :json}, { :user_id => users(:fooBar).id })
-    assert_equal ActiveSupport::JSON.decode(response.body)["files"], []
+    assert_equal files(response), []
   end
 
   test "search for a filetype" do
     response = post(:find, {:tags => [], :filetypes => [bruse_files(:one).filetype], :fuzzy => [], :format => :json}, { :user_id => users(:fooBar).id })
-    assert_equal ActiveSupport::JSON.decode(response.body)["files"].first["name"], bruse_files(:one).name
+    assert_equal files(response).first["name"], bruse_files(:one).name
   end
 
   test "search for a tag name" do
     response = post(:find, {:tags => [tags(:one).name], :filetypes => [], :fuzzy => [], :format => :json}, { :user_id => users(:fooBar).id })
-    assert_equal ActiveSupport::JSON.decode(response.body)["files"].first["name"], bruse_files(:one).name
+    assert_equal files(response).first["name"], bruse_files(:one).name
   end
 
   test "trying to search for files that the user doesn't own" do
     response = post(:find, {:tags => [], :filetypes => [], :fuzzy => [bruse_files(:one).name], :format => :json}, { :user_id => users(:arbar).id })
-    assert_equal ActiveSupport::JSON.decode(response.body)["files"], []
+    assert_equal files(response), []
+  end
+
+  test "search with several criterias" do
+    response = post(:find, {:tags => [tags(:one).name], :filetypes => [bruse_files(:one).filetype], :fuzzy => [bruse_files(:one).name], :format => :json}, { :user_id => users(:fooBar).id })
+    assert_equal files(response).first["name"], bruse_files(:one).name
   end
 end
