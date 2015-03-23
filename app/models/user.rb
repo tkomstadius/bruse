ed4bd864b83ff9ab8cc3add226eb29c44a8abca2
danielronnkvist <daniel@ronnkvist.com>
2015-03-23 13:59:22 +0100

    Generated the user model to be a devise model
    Made a seperate migration for those columns that already existed.

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,8 @@
 class User < ActiveRecord::Base
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable and :omniauthable
+  devise :database_authenticatable, :registerable, :omniauthable,
+         :recoverable, :rememberable, :trackable, :validatable, :confirmable
   # relations
   has_many :identities
 
