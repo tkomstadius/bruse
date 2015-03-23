65d7003090972a2076d56bbba926d012ff8d6365
danielronnkvist <daniel@ronnkvist.com>
2015-03-23 10:40:04 +0100

    Moved assertion to be done after each test

--- test/controllers/search_controller_test.rb
+++ test/controllers/search_controller_test.rb
@@ -6,24 +6,23 @@ class SearchControllerTest < ActionController::TestCase
     Tag.bulk_update_fuzzy_name
   end
 
+  after do
+    assert_response :success
+    assert_template :find
+  end
+
   test "fuzzy search for a file name" do
     response = post(:find, {:tags => [], :filetypes => [], :fuzzy => [bruse_files(:one).name], :format => :json}, { :user_id => users(:fooBar).id })
     assert_includes response.body, bruse_files(:one).name
-    assert_response :success
-    assert_template :find
   end
 
   test "search with empty parameters" do
     response = post(:find, {:tags => [], :filetypes => [], :fuzzy => [], :format => :json}, { :user_id => users(:fooBar).id })
     assert_equal ActiveSupport::JSON.decode(response.body)["files"], []
-    assert_response :success
-    assert_template :find
   end
 
   test "search for a filetype" do
     response = post(:find, {:tags => [], :filetypes => [bruse_files(:one).filetype], :fuzzy => [], :format => :json}, { :user_id => users(:fooBar).id })
     assert_equal ActiveSupport::JSON.decode(response.body)["files"].first["name"], bruse_files(:one).name
-    assert_response :success
-    assert_template :find
   end
 end
