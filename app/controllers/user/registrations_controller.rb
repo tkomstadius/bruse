a76443c6a70b64f50fa76e735aee09ac0443a6e3
danielronnkvist <daniel@ronnkvist.com>
2015-03-31 21:01:18 +0200

    Allow users without password to update their profile

--- app/controllers/user/registrations_controller.rb
+++ app/controllers/user/registrations_controller.rb
@@ -1,5 +1,16 @@
 class User::RegistrationsController < Devise::RegistrationsController
 
+  protected
+
+  def update_resource(resource, params)
+    if !resource.own_password
+      params.delete(:current_password)
+      resource.update_without_password(params)
+    else
+      resource.update_with_password(params)
+    end
+  end
+
   private
 
   def sign_up_params
