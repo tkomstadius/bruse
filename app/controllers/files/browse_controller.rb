e4f075f8015e120ffe4f3e9a3b2d8e3fcffcb683
tkomstadius <t.komstadius@gmail.com>
2015-04-20 14:21:22 +0200

    added new route to upload function

--- app/controllers/files/browse_controller.rb
+++ app/controllers/files/browse_controller.rb
@@ -3,7 +3,7 @@ class Files::BrowseController < Files::FilesController
   skip_before_filter :set_identity, only: :upload
   # getting 'can't verify CSRF token authenticity'
   # this could be a serius problem in security?
-  # skip_before_filter :verify_authenticity_token, only: [:upload_from_base64]
+  skip_before_filter :verify_authenticity_token, only: [:upload_from_base64]
   require 'base64'
 
   def browse
@@ -50,10 +50,20 @@ class Files::BrowseController < Files::FilesController
     IO.write(params[:name], decoded_content)
 
     if params[:location] == 'local'
-      BruseFile.new(name: params[:name],
-                    foreign_ref: fileref,
-                    filetype: params[:type],
-                    identity: current_user.identities.find_by(:service => "local"))
+      file = BruseFile.new
+      
+      file.name =  params[:name],
+      file.foreign_ref = fileref,
+      file.filetype = params[:type],
+      file.identity =  current_user.identities.find_by(:service => "local")
+      
+      if file.save #file.identity.bruse_files << file
+        flash[:notice] = "#{file.name} was saved!"
+        redirect_to bruse_files_path
+      else
+        flash[:notice] = "you must log in to your bruse acount!"
+        redirect_to bruse_files_path
+    end
     elsif params[:location] == 'dropbox'
     elsif params[:location] == 'drive'
     else
