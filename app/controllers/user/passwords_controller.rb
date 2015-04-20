8d8986328c23d276a1c95c985247a77823c6c3bb
danielronnkvist <daniel@ronnkvist.com>
2015-04-20 14:25:42 +0200

    Hijacked the after_resetting_password_path_for function and updates the user object

--- app/controllers/user/passwords_controller.rb
+++ app/controllers/user/passwords_controller.rb
@@ -3,8 +3,14 @@ class User::PasswordsController < Devise::PasswordsController
   before_filter :require_no_password, only: [:new, :create, :edit, :update]
 
   protected
+  def after_resetting_password_path_for(resource)
+    resource.own_password = true
+    resource.save!
+    after_sign_in_path_for(resource)
+  end
+
   def require_no_password
-    if current_user.own_password
+    if current_user && current_user.own_password
       require_no_authentication
     end
   end
