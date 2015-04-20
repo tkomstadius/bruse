6e3a555b8f8841a54b358ab777a11dd959320139
danielronnkvist <daniel@ronnkvist.com>
2015-04-20 13:54:02 +0200

    Before filter that checks if the user has a password

--- app/controllers/user/passwords_controller.rb
+++ app/controllers/user/passwords_controller.rb
@@ -1,3 +1,11 @@
 class User::PasswordsController < Devise::PasswordsController
   skip_before_filter :require_no_authentication, only: [:new, :create, :edit, :update]
+  before_filter :require_no_password, only: [:new, :create, :edit, :update]
+
+  protected
+  def require_no_password
+    if current_user.own_password
+      require_no_authentication
+    end
+  end
 end
