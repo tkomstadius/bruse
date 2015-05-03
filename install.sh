e28dc435bc5b7343060eec712682e371f9da2a2f
danielronnkvist <daniel@ronnkvist.com>
2015-05-03 16:14:54 +0200

    Setup psql and rails production key

--- install.sh
+++ install.sh
@@ -7,7 +7,12 @@ if type "psql -V" > /dev/null 2>&1; then
 fi
 
 # setup psql credentials
+psql -c "CREATE USER bruse WITH PASSWORD 'kex2015'"
+psql -c "CREATE DATABASE bruse_production WITH OWNER bruse"
 
 curl -L https://github.com/tkomstadius/bruse/archive/master.tar.gz | tar xz --strip-components=1
 bundle install
-rails s
+
+export SECRET_KEY_BASE=randomstring
+# setup database.yml
+rails s -e production
