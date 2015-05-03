04ff19e0ec8abce077d7836624992d1ed6db9304
danielronnkvist <daniel@ronnkvist.com>
2015-05-03 15:46:28 +0200

    Checks if ruby and pswl is installed

--- install.sh
+++ install.sh
@@ -1,3 +1,13 @@
+if type "rvm -v" > /dev/null 2>&1; then
+  echo "install ruby"
+fi
+
+if type "psql -V" > /dev/null 2>&1; then
+  echo "install psql"
+fi
+
+# setup psql credentials
+
 curl -L https://github.com/tkomstadius/bruse/archive/master.tar.gz | tar xz --strip-components=1
 bundle install
 rails s
