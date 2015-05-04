ead7efaf3fd69db910a4ccfb31d93b8a734280f3
danielronnkvist <daniel@ronnkvist.com>
2015-05-04 15:09:22 +0200

    Generates a random string
    Runs the setup file instead of bundle install

--- install.sh
+++ install.sh
@@ -11,8 +11,11 @@ psql -c "CREATE USER bruse WITH PASSWORD 'kex2015'"
 psql -c "CREATE DATABASE bruse_production WITH OWNER bruse"
 
 curl -L https://github.com/tkomstadius/bruse/archive/master.tar.gz | tar xz --strip-components=1
-bundle install
 
-export SECRET_KEY_BASE=randomstring
-# setup database.yml
+# run the setup file
+./bin/setup
+
+# Generate a random key
+export SECRET_KEY_BASE=$(date +%s | sha256sum | base64 | head -c 32 ; echo)
+
 rails s -e production
