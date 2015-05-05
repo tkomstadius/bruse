585f43d177ff4741d93710ea58b071fd5c1dafee
danielronnkvist <daniel@ronnkvist.com>
2015-05-05 09:39:10 +0200

    Creates a devise config file depending on what is choosen

--- install.sh
+++ install.sh
@@ -20,6 +20,8 @@ curl -L https://github.com/tkomstadius/bruse/archive/master.tar.gz | tar xz --st
 # Generate a random key
 export SECRET_KEY_BASE=$(date +%s | sha256sum | base64 | head -c 32 ; echo)
 
+printf "Devise.setup do |config|\n\n  config.mailer_sender = 'please-change-me-at-config-initializers-devise@example.com'\n\n  require 'devise/orm/active_record'\n\n  config.case_insensitive_keys = [ :email ]\n\n  config.strip_whitespace_keys = [ :email ]\n\n  config.skip_session_storage = [:http_auth]\n\n  config.stretches = Rails.env.test? ? 1 : 10\n\n  config.allow_unconfirmed_access_for = 3.days\n\n  config.reconfirmable = true\n\n  config.expire_all_remember_me_on_sign_out = true\n\n  config.password_length = 8..128\n\n  config.reset_password_within = 6.hours\n\n  config.sign_out_via = :delete\n\n" > config/initializers/devise.rb
+
 promptForDropbox(){
   echo "Enter your DROPBOX_KEY: "
   read db_key
@@ -30,6 +32,8 @@ promptForDropbox(){
   export DROPBOX_SECRET=$db_secret
   printf "DROPBOX_SECRET: '$db_secret'\n" >> config/application.yml
   thirdParty=true
+
+  printf "config.omniauth :dropbox_oauth2, ENV['DROPBOX_KEY'], ENV['DROPBOX_SECRET']\n" >> config/initializers/devise.rb
 }
 while true; do
   read -p "Do you want to use Dropbox? (y/n)" yn
@@ -50,6 +54,8 @@ promptForDrive(){
   export DRIVE_SECRET=$d_secret
   printf "DRIVE_SECRET: '$d_secret'\n" >> config/application.yml
   thirdParty=true
+
+  printf "config.omniauth :google_oauth2, ENV['DRIVE_KEY'], ENV['DRIVE_SECRET'],\n{ :scope => 'email, profile, https://www.googleapis.com/auth/drive'}\n" >> config/initializers/devise.rb
 }
 while true; do
   read -p "Do you want to use Google Drive? (y/n)" yn
@@ -59,6 +65,8 @@ while true; do
     * ) echo "Please answer yes or no.";;
   esac
 done
+
+printf "end" >> config/initializers/devise.rb
 
 if [ "$thirdParty" = true ] ; then
   echo "You can allways change your codes by editing the config/application.yml."
