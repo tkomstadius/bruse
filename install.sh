e9d2a7e0d2624f952831a07aec69c94aacf82f16
danielronnkvist <daniel@ronnkvist.com>
2015-05-06 15:03:24 +0200

    Skips /bin/setup and runs the project with foreman
    Checks if foreman is installed, if not it will install it(hopefully). Creates a Procfile with the standard rails server in production mode.

--- install.sh
+++ install.sh
@@ -1,5 +1,6 @@
 if type "rvm -v" > /dev/null 2>&1; then
   echo "Install ruby before continuing"
+  echo "Perhaps by running this: curl -sSL https://get.rvm.io | bash -s stable --rails"
   exit
 fi
 
@@ -10,27 +11,28 @@ fi
 
 # setup psql credentials
 PASSWORD=$(date +%s | sha256sum | base64 | head -c 32 ; echo)
-sudo su - postgres -c "psql -c \"CREATE USER bruse WITH PASSWORD '$PASSWORD'\""
-sudo su - postgres -c "psql -c \"CREATE DATABASE bruse_production WITH OWNER bruse\""
+sudo su - postgres -c "psql -c \"CREATE USER cool_bruse WITH PASSWORD '$PASSWORD' SUPERUSER\""
+sudo su - postgres -c "psql -c \"CREATE DATABASE bruse_production WITH OWNER cool_bruse\""
 curl -L https://github.com/tkomstadius/bruse/archive/develop.tar.gz | tar xz --strip-components=1
 
-# Update the database config file
+# Update the database config filefile
 printf "production:\n  <<: *default\n  username: cool_bruse\n  password: $PASSWORD\n  database: bruse_production" >> config/database.yml
 
 # run the setup file
-./bin/setup
+bundle install
+rake db:setup
 
 # Generate a random key
 export SECRET_KEY_BASE=$(date +%s | sha256sum | base64 | head -c 32 ; echo)
 
-printf "Devise.setup do |config|\n\n  config.mailer_sender = 'please-change-me-at-config-initializers-devise@example.com'\n\n  require 'devise/orm/active_record'\n\n  config.case_insensitive_keys = [ :email ]\n\n  config.strip_whitespace_keys = [ :email ]\n\n  config.skip_session_storage = [:http_auth]\n\n  config.stretches = Rails.env.test? ? 1 : 10\n\n  config.allow_unconfirmed_access_for = 3.days\n\n  config.reconfirmable = true\n\n  config.expire_all_remember_me_on_sign_out = true\n\n  config.password_length = 8..128\n\n  config.reset_password_within = 6.hours\n\n  config.sign_out_via = :delete\n\n" > config/initializers/devise.rb
+printf "Devise.setup do |config|\n\n  config.secret_key = 'a9050a40481fe4f8d864147fc7c7e56a2bd5dfbeb079dff3c05b745286da9315ed9a40e511cf83c57db26deaec4949cdc6ca8cef1e530dd337edb3c8184a1f31'\n\n  config.mailer_sender = 'please-change-me-at-config-initializers-devise@example.com'\n\n  require 'devise/orm/active_record'\n\n  config.case_insensitive_keys = [ :email ]\n\n  config.strip_whitespace_keys = [ :email ]\n\n  config.skip_session_storage = [:http_auth]\n\n  config.stretches = Rails.env.test? ? 1 : 10\n\n  config.allow_unconfirmed_access_for = 3.days\n\n  config.reconfirmable = true\n\n  config.expire_all_remember_me_on_sign_out = true\n\n  config.password_length = 8..128\n\n  config.reset_password_within = 6.hours\n\n  config.sign_out_via = :delete\n\n" > config/initializers/devise.rb
 
 promptForDropbox(){
-  echo "Enter your DROPBOX_KEY: "
+  echo -n "Enter your DROPBOX_KEY: "
   read db_key
   export DROPBOX_KEY=$db_key
   printf "DROPBOX_KEY: '$db_key'\n" >> config/application.yml
-  echo "Enter your DROPBOX_SECRET: "
+  echo -n "Enter your DROPBOX_SECRET: "
   read db_secret
   export DROPBOX_SECRET=$db_secret
   printf "DROPBOX_SECRET: '$db_secret'\n" >> config/application.yml
@@ -48,11 +50,11 @@ while true; do
 done
 
 promptForDrive(){
-  echo "Enter your DRIVE_KEY: "
+  echo -n "Enter your DRIVE_KEY: "
   read d_key
   export DRIVE_KEY=$d_key
   printf "DRIVE_KEY: '$d_key'\n" >> config/application.yml
-  echo "Enter your DRIVE_SECRET: "
+  echo -n "Enter your DRIVE_SECRET: "
   read d_secret
   export DRIVE_SECRET=$d_secret
   printf "DRIVE_SECRET: '$d_secret'\n" >> config/application.yml
@@ -75,4 +77,18 @@ if [ "$thirdParty" = true ] ; then
   echo "You can allways change your codes by editing the config/application.yml."
 fi
 
-rails s -e production
+if type "foreman -v" > /dev/null 2>&1; then
+  sudo apt-get install ca-certificates
+  wget https://apt.puppetlabs.com/puppetlabs-release-trusty.deb
+  dpkg -i puppetlabs-release-trusty.deb
+
+  echo "deb http://deb.theforeman.org/ trusty 1.8" > /etc/apt/sources.list.d/foreman.list
+  echo "deb http://deb.theforeman.org/ plugins 1.8" >> /etc/apt/sources.list.d/foreman.list
+  wget -q http://deb.theforeman.org/pubkey.gpg -O- | apt-key add -
+
+  sudo apt-get update && apt-get install foreman-installer
+  foreman-installer
+fi
+echo "web: rails s -e production" > Procfile
+
+foreman start
