04a874c7b3cf933e2338924ff4075acbaa9da117
danielronnkvist <daniel@ronnkvist.com>
2015-05-11 09:04:46 +0200

    Updated with some try and catch blocks.

--- install.sh
+++ install.sh
@@ -1,27 +1,28 @@
-if type "rvm -v" > /dev/null 2>&1; then
-  echo "Install ruby before continuing"
-  echo "Perhaps by running this: curl -sSL https://get.rvm.io | bash -s stable --rails"
-  exit
-fi
-
-if type "psql -V" > /dev/null 2>&1; then
-  echo "Install psql before continuing"
-  exit
-fi
-
-# setup psql credentials
 PASSWORD=$(date +%s | sha256sum | base64 | head -c 32 ; echo)
-sudo su - postgres -c "psql -c \"CREATE USER cool_bruse WITH PASSWORD '$PASSWORD' SUPERUSER\""
-sudo su - postgres -c "psql -c \"CREATE DATABASE bruse_production WITH OWNER cool_bruse\""
-curl -L https://github.com/tkomstadius/bruse/archive/develop.tar.gz | tar xz --strip-components=1
-
+{
+  # setup psql credentials
+  sudo su - postgres -c "psql -c \"CREATE USER cool_bruse WITH PASSWORD '$PASSWORD' SUPERUSER\""
+  sudo su - postgres -c "psql -c \"CREATE DATABASE bruse_production WITH OWNER cool_bruse\""
+} || {
+  echo "Error trying to create databse credentials. Make sure Postgres is installed and that the user with admin rights is named postgres."
+  exit
+}
+{
+  curl -L https://github.com/tkomstadius/bruse/archive/develop.tar.gz | tar xz --strip-components=1
+} || {
+  echo "Have you installed curl?"
+  exit
+}
 # Update the database config filefile
 printf "production:\n  <<: *default\n  username: cool_bruse\n  password: $PASSWORD\n  database: bruse_production" >> config/database.yml
-
-# run the setup file
-bundle install
-rake db:setup
-
+{
+  # run the setup file
+  bundle install
+  rake db:setup
+} || {
+  echo "Is Rails setup correctly?"
+  exit
+}
 # Generate a random key
 export SECRET_KEY_BASE=$(date +%s | sha256sum | base64 | head -c 32 ; echo)
 
@@ -77,18 +78,22 @@ if [ "$thirdParty" = true ] ; then
   echo "You can allways change your codes by editing the config/application.yml."
 fi
 
-if type "foreman -v" > /dev/null 2>&1; then
-  sudo apt-get install ca-certificates
-  wget https://apt.puppetlabs.com/puppetlabs-release-trusty.deb
-  dpkg -i puppetlabs-release-trusty.deb
+{
+  if type "foreman -v" > /dev/null 2>&1; then
+    sudo apt-get install ca-certificates
+    wget https://apt.puppetlabs.com/puppetlabs-release-trusty.deb
+    dpkg -i puppetlabs-release-trusty.deb
 
-  echo "deb http://deb.theforeman.org/ trusty 1.8" > /etc/apt/sources.list.d/foreman.list
-  echo "deb http://deb.theforeman.org/ plugins 1.8" >> /etc/apt/sources.list.d/foreman.list
-  wget -q http://deb.theforeman.org/pubkey.gpg -O- | apt-key add -
+    echo "deb http://deb.theforeman.org/ trusty 1.8" > /etc/apt/sources.list.d/foreman.list
+    echo "deb http://deb.theforeman.org/ plugins 1.8" >> /etc/apt/sources.list.d/foreman.list
+    wget -q http://deb.theforeman.org/pubkey.gpg -O- | apt-key add -
 
-  sudo apt-get update && apt-get install foreman-installer
-  foreman-installer
-fi
-echo "web: rails s -e production" > Procfile
+    sudo apt-get update && apt-get install foreman-installer
+    foreman-installer
+  fi
+  echo "web: rails s -e production" > Procfile
 
-foreman start
+  foreman start
+} || {
+  echo "Please install foreman."
+}
