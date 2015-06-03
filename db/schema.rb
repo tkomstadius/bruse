3de18c2582214bae19c79f808e538580633a5ba1
Daniel Rönnkvist <daniel@ronnkvist.com>
2015-06-03 14:36:44 +0200

    Store expire_at in db

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150603071359) do
+ActiveRecord::Schema.define(version: 20150603123601) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -58,6 +58,7 @@ ActiveRecord::Schema.define(version: 20150603071359) do
     t.string   "uid"
     t.string   "name"
     t.string   "refresh_token"
+    t.integer  "expires_at"
   end
 
   add_index "identities", ["user_id"], name: "index_identities_on_user_id", using: :btree
